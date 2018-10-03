7d7e23a640cff46b46a6530090f97b2d83021345
Tamás Hugyák <tamas.hugyak@outlook.com>
2018-10-03 15:44:43 +0200

    feat(pages): add PageNotFound and Welcome pages to the app

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+
+const routes: Routes = [
+  { path: 'main', component: WelcomeComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {useHash: true})],
