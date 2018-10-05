2b9b63276c05d1b82b267f51b780b695add3d26e
Tamás Hugyák <tamas.hugyak@outlook.com>
2018-10-05 12:22:12 +0200

    feat(server): handle the CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { CORSMiddleware } from './utils';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(CORSMiddleware);
   await app.listen(3000);
 }
 bootstrap();
--- server/src/utils.ts
+++ server/src/utils.ts
@@ -13,3 +13,14 @@ export function parseNumber(value) {
     return value;
   }
 }
+
+export function CORSMiddleware(req, res, next) {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  if (req.method === 'OPTIONS') {
+    res.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE');
+    res.send('');
+  } else {
+    next();
+  }
+}
