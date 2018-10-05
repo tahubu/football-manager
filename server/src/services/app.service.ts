dd14f47bccb89254274adb2fa1645ad8a2f9c89e
Tamás Hugyák <tamas.hugyak@outlook.com>
2018-10-05 10:41:17 +0200

    refact(store-init): add the default teams to the store

--- server/src/services/app.service.ts
+++ server/src/services/app.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
-import { players } from '../data/init-store';
+import { players, teams } from '../data/init-store';
 import { PlayerService } from './player.service';
+import { TeamService } from './team.service';
 
 @Injectable()
 export class AppService {
-  constructor(private playerService: PlayerService) {
+  constructor(private playerService: PlayerService, private teamService: TeamService) {
   }
 
   public initStore(): void {
     // add each player to the Store
     players.forEach(player => this.playerService.addPlayer(player));
+    // add each team to the Store
+    teams.forEach(team => this.teamService.addTeam(team));
   }
 }
