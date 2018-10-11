3f94c698c325fd31af1592110ec377d073f6e483
Tamás Hugyák <tamas.hugyak@outlook.com>
2018-10-11 16:25:56 +0200

    refact(files): remove files

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,17 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { API_URL } from './constants/api-url.constant';
+import { API_URL } from '@constants/api-url.constant';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Error404PageComponent } from './pages/error-404-page/error-404-page.component';
-import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
-import { TeamPageComponent } from './pages/team-page/team-page.component';
-import { ListItemsComponent } from './components/list-items/list-items.component';
-import { PlayersPageComponent } from './pages/players-page/players-page.component';
-import { AddPlayerComponent } from './pages/players-page/add-player/add-player.component';
-import { ModifyPlayerComponent } from './pages/players-page/modify-player/modify-player.component';
-import { FormatColumnNamePipe } from './pipes/format-column-name.pipe';
+import { Error404PageComponent } from '@pages/error-404-page/error-404-page.component';
+import { WelcomePageComponent } from '@pages/welcome-page/welcome-page.component';
+import { ListItemsComponent } from '@components/list-items/list-items.component';
+import { FormatColumnNamePipe } from '@pipes/format-column-name.pipe';
 
 @NgModule({
   declarations: [
@@ -21,10 +17,6 @@ import { FormatColumnNamePipe } from './pipes/format-column-name.pipe';
     Error404PageComponent,
     WelcomePageComponent,
     ListItemsComponent,
-    TeamPageComponent,
-    PlayersPageComponent,
-    AddPlayerComponent,
-    ModifyPlayerComponent,
     FormatColumnNamePipe,
   ],
   imports: [
